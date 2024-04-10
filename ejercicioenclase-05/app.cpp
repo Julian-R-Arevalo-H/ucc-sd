b439a3d416d15351ac35e07bf1c241905d82e249
https://github.com/Julian-R-Arevalo-H/pruebagit.git <julianarevalohis20241@gmail.com>
2024-04-10 12:40:38 -0500

    Primer ejercicio de Abril

---
+++ ejercicioenclase-05/app.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define PI 3.1416
+
+int main(){
+	float r,L;
+	
+	printf("Ingrese Radio: ");
+	scanf("%f",&r);
+	
+	L=PI*r;
+	
+	printf("La longitud es: %f",L);
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
+
+
